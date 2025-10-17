e8605f1cbbda5aad4e15f0500b465c22d4fe4b23
suzhidong <suzhidong1314@gmail.com>
2025-10-17 13:35:53 -0500

    first try exercise 1

--- Lab4/Lab4EX1.cpp
+++ Lab4/Lab4EX1.cpp
@@ -37,19 +37,33 @@ int main(){
 
 		/*Read the bytes containing the bumper, wheel drop,
 			and cliff sensors. You can convert them into a usable data type.*/
-
+		char bumper=serialGetchar(kobuki);
+		char drop=serialGetchar(kobuki);
+		char cliff= serialGetchar(kobuki);
 		/*Print the data to the screen.*/
-
+		cout<<"Left bumper ="<<(bumper&0x01)<<" Center bumper ="<<(bumper&0x02)<<" Right bumper = "<<(bumper&0x04)<<endl;
+		cout<<"Left wheel ="<<(drop&0x01)<<" Center wheel ="<<(drop&0x02)<<" Right wheel = "<<(drop&0x04)<<endl;
+		cout<<"Left cliff ="<<(cliff&0x01)<<" Center cliff ="<<(cliff&0x02)<<" Right cliff = "<<(cliff&0x04)<<endl;
 		/*Read through 6 bytes between the cliff sensors and
 		the button sensors.*/
-
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
 		/*Read the byte containing the button data.*/
-
+		char button = serialGetchar(kobuki);
 		/*Close the script and the connection to the Kobuki when
 		Button 1 on the Kobuki is pressed. Use serialClose(kobuki);*/
+		if(button & 0x02){
+			serialClose(kobuki);
+			break;
+		}
 
-
-		
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
+		serialGetchar(kobuki);
 
 		//Pause the script so the data read receive rate is the same as the Kobuki send rate.
 		usleep(20000);
